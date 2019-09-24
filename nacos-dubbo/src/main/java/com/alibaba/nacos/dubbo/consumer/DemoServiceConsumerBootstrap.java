9c0f943b67f0f14310f3c35ab8c7e94633c145c9
pengzhe <zhe.peng@alibaba-inc.com>
2019-09-24 19:40:13 +0800

    dubbo

--- nacos-dubbo/src/main/java/com/alibaba/nacos/dubbo/consumer/DemoServiceConsumerBootstrap.java
+++ nacos-dubbo/src/main/java/com/alibaba/nacos/dubbo/consumer/DemoServiceConsumerBootstrap.java
@@ -20,7 +20,7 @@ public class DemoServiceConsumerBootstrap {
 
     @PostConstruct
     public void init() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000000; i++) {
             System.out.println(demoService.sayName("哲哥）"));
         }
     }
