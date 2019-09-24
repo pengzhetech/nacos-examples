0eec91ef3cda96f35f21f012a04eb459f086381d
pengzhe <zhe.peng@alibaba-inc.com>
2019-09-24 17:56:51 +0800

    nacos-spring-boot-config-example is ok

--- nacos-spring-boot-example/nacos-spring-boot-config-mysql-example/src/main/java/com/alibaba/nacos/example/spring/boot/bean/Beans.java
+++ nacos-spring-boot-example/nacos-spring-boot-config-mysql-example/src/main/java/com/alibaba/nacos/example/spring/boot/bean/Beans.java
@@ -5,11 +5,13 @@ import com.alibaba.nacos.example.spring.boot.nacos.NacosConfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author pengzhe
  * @date 2019-09-24 17:29
  */
+@Configuration
 public class Beans {
 
     @Autowired
